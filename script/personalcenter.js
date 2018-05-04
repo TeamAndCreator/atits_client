8e21b80805d32236eaa2d8e142f7dfab1d5a7f85
Mosery <1169313164@qq.com>
2018-05-04 10:58:55 +0800

    登出

--- script/personalcenter.js
+++ script/personalcenter.js
@@ -2,65 +2,27 @@ apiready = function() {
 
 }
 
-// $(".logout").on("click", function() {
-//     // alert("123");
-//     $.ajax({
-//         url: 'http://47.104.26.79:8080/atits_service/login/logout ',
-//         type: 'get',
-//         dataType: 'json',
-//         success: function(result) {
-//             if (result.code == 100) {
-//               // $api.setStorage('userInfo', 'result');
-//               api.confirm({
-//                   title: '提示',
-//                   msg: '是否退出登录？',
-//                   buttons: ['确定', '取消']
-//               }, function(ret, err){
-//                   if( ret ){
-//                     api.openWin({
-//                         name: 'login',
-//                         url: './login.html',
-//                         pageParam: {
-//                             name: 'test'
-//                         }
-//                     });
-//                   }else{
-//                        alert( JSON.stringify( err ) );
-//                   }
-//               });
-//                 //
-//             } else {
-//                 alert(result.msg);
-//             }
-//         }
-//     });
-// });
-function logout() {
-    api.confirm({
-        title: '提示',
-        msg: '是否退出登录',
-        buttons: ['确定', '取消']
-    }, function(ret, err) {
-        if (ret) {
-            $api.rmStorage('userInfo');
+$(".logout").on("click", function() {
+    // alert("123");
+    $.ajax({
+        url: 'http://47.104.26.79:8080/atits_service/login/logout ',
+        type: 'get',
+        dataType: 'json',
+        success: function(result) {
+            if (result.code == 100) {
+                $api.rmStorage('userInfo');
+                // $api.setStorage('userInfo', 'result');
 
-            api.toast({
-                msg: '退出登录成功',
-                duration: 2000,
-                location: 'bottom'
-            });
-
-            api.openWin({
-                name: 'login',
-                url: './login.html',
-                pageParam: {
-                    name: 'test'
-                }
-            });
-
-        } else {
-            alert(JSON.stringify(err));
+                api.openWin({
+                    name: 'login',
+                    url: './login.html',
+                    pageParam: {
+                        name: 'test'
+                    }
+                });
+            } else {
+                alert(result.msg);
+            }
         }
     });
-
-}
+});
