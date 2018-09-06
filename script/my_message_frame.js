ce6fc3c4fb1469c49bd86aebb653bcc8835b49f5
hzh macbook pro <apple@hzhdeMacBook-Pro.local>
2018-09-06 21:41:48 +0800

    modified login.css

--- script/my_message_frame.js
+++ script/my_message_frame.js
@@ -10,9 +10,9 @@ apiready = function(){
           dataType: 'json',
           success: function(result){
               if(result.code == 100){
-                  console.log(JSON.stringify(result));
+                  //console.log(JSON.stringify(result));
                   $('#userName').html($api.getStorage('userName').replace(/\"/g, ""));
-                  $('#name').html(JSON.stringify(result.data.profile.name));
+                  $('#name').html(JSON.stringify(result.data.profile.name).replace(/\"/g, ""));
                   $('#phoneNumber').html(JSON.stringify(result.data.profile.phoneNumber));
                   $('#officePhone').html(JSON.stringify(result.data.profile.officePhone));
                   $('#email').html(JSON.stringify(result.data.profile.email));
